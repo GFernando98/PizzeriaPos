4f18039340238f90c76d0fcaad8ae084d8e6f1c8
German Mendoza <118017795+GFernando98@users.noreply.github.com>
2024-05-25 16:07:26 -0600

    Create Script.sql

---
+++ SQL Server/Script.sql
@@ -0,0 +1,59 @@
+﻿
+--Creacion de base de datos
+CREATE DATABASE POSPizzeria
+
+--Usar base de datos
+USE POSPizzeria
+
+-- Creacion de modelos/tablas
+CREATE TABLE Cliente (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nombre NVARCHAR(255) NOT NULL,
+    Apellido NVARCHAR(255) NOT NULL,
+    Email NVARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE Direccion (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    ClienteId INT NOT NULL,
+    Calle NVARCHAR(255) NOT NULL,
+    Numero INT NOT NULL,
+    Ciudad NVARCHAR(255) NOT NULL,
+    Pais NVARCHAR(255) NOT NULL,
+    FOREIGN KEY (ClienteId) REFERENCES Cliente(Id)
+);
+
+CREATE TABLE Producto (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nombre NVARCHAR(255) NOT NULL,
+    Descripcion NVARCHAR(500) NULL,
+    Precio DECIMAL(10,2) NOT NULL
+);
+
+CREATE TABLE Pedido (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    ClienteId INT NOT NULL,
+    FechaPedido DATE NOT NULL,
+    TotalPedido DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (ClienteId) REFERENCES Cliente(id)
+);
+
+CREATE TABLE PedidoDetalle (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    PedidoId INT NOT NULL,
+    ProductoId INT NOT NULL,
+    Cantidad INT NOT NULL,
+    PrecioUnitario DECIMAL(10,2) NOT NULL,
+    Impuesto DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (ProductoId) REFERENCES Producto(Id),
+    FOREIGN KEY (PedidoId) REFERENCES Pedido(Id)
+);
+
+
+CREATE TABLE Usuario (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    UserName NVARCHAR(50) NOT NULL,
+    Password BINARY(64) NOT NULL
+);
+
+
